82de8bd150ae9f082fd92f555fa4e29077e24051
JoeC <cpaul@pdx.edu>
2016-07-01 00:07:27 -0700

    Added the Game Manager class
    The class currently has no functionality, this is mostly
    to test git and Android Studio working together properly.

---
+++ app/src/main/java/oss2016/pixelforts/GameManager.java
@@ -0,0 +1,13 @@
+package oss2016.pixelforts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_manager);
+    }
+}
