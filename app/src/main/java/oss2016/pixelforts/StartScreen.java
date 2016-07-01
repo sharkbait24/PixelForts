c57776f54d92b8d95ecd9787f9730314a7dbbb5c
JoeC <cpaul@pdx.edu>
2016-06-30 23:45:38 -0700

    More license information in files

--- app/src/main/java/oss2016/pixelforts/StartScreen.java
+++ app/src/main/java/oss2016/pixelforts/StartScreen.java
@@ -3,6 +3,9 @@ package oss2016.pixelforts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/* Copyright (c) 2016 Joe Coleman
+   This program is available under the "MIT" license.
+   Please see the COPYING file for license information.*/
 public class StartScreen extends AppCompatActivity {
 
     @Override
