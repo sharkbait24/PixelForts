713c9a86afb19d0dff71239bbff6233be949388f
joe <cpaul@pdx.edu>
2016-07-05 15:10:50 -0700

    Setup basic OpenGL rendering of background in GameManager.
    Added basic black screen background rendering in OpenGL to GameManager.  This will be moved to a separate class once I'm more comfortable with the OpenGL api.

--- app/src/main/java/oss2016/pixelforts/GameManager.java
+++ app/src/main/java/oss2016/pixelforts/GameManager.java
@@ -1,16 +1,59 @@
 package oss2016.pixelforts;
 
+import android.content.Context;;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.egl.EGLConfig;
 
 /* Copyright (c) 2016 Joe Coleman
    This program is available under the "MIT" license.
    Please see the COPYING file for license information.*/
 public class GameManager extends AppCompatActivity {
+    private GLSurfaceView gmView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_manager);
+
+        // Create a GLSurfaceVIew instance and set it
+        // as the ContentView for this Activity.
+        gmView = new GMGLSurfaceView(this);
+        setContentView(gmView);
+    }
+}
+
+class GMGLSurfaceView extends GLSurfaceView {
+    private final GMGLRenderer gmRenderer;
+
+    public GMGLSurfaceView(Context context){
+        super(context);
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+
+        gmRenderer = new GMGLRenderer();
+
+        // Set the Renderer for drawing on the GMGLSurfaceView
+        setRenderer(gmRenderer);
+    }
+}
+
+class GMGLRenderer implements GLSurfaceView.Renderer {
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    }
+
+    public void onDrawFrame(GL10 unused) {
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
     }
 }
