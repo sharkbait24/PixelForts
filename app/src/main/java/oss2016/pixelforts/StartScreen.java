aaa8f99772f8bc75a2b99601df4a8a0d6c29c1e1
joe <cpaul@pdx.edu>
2016-07-05 13:28:46 -0700

    Added startGame function to StartScreen and made the start button call it.
    Added a new public function in StartScreen.java that will be called by the start button.  This function will setup and call the GameManager to start the game.

--- app/src/main/java/oss2016/pixelforts/StartScreen.java
+++ app/src/main/java/oss2016/pixelforts/StartScreen.java
@@ -1,7 +1,9 @@
 package oss2016.pixelforts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 /* Copyright (c) 2016 Joe Coleman
    This program is available under the "MIT" license.
@@ -12,5 +14,11 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+    }
+
+    /* Called when the user clicks the start game button */
+    public void startGame(View view){
+        Intent intent = new Intent(this, GameManager.class);
+        startActivity(intent);
     }
 }
